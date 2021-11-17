139eed52224cb361c3f122ee581cf9d9030f7418
Colby Workman <cgw2319@gmail.com>
2021-11-17 14:27:38 -0700

    alert added

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 console.log('hello world')
+
+let siteBtn = document.getElementById('site-btn')
+
+const fakeError = () => {
+    alert('Our site is undergoing temporary maintenance')
+}
+
+siteBtn.addEventListener('click', fakeError)
